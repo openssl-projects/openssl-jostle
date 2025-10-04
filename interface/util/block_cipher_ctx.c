5e4af2d60feeb3fd9114312ffdce4ab5057b2117
Tim Hudson <tjh@cryptsoft.com>
2025-10-04 23:35:37 +1000

    tweak copyright notices

--- interface/util/block_cipher_ctx.c
+++ interface/util/block_cipher_ctx.c
@@ -1,9 +1,9 @@
-//  Copyright 2005-2025 The OpenSSL Project Authors. All Rights Reserved.
+//  Copyright 2025 OpenSSL Jostle Authors. All Rights Reserved.
 //
 //  Licensed under the Apache License 2.0 (the "License"). You may not use
 //  this file except in compliance with the License.  You can obtain a copy
 //  in the file LICENSE in the source distribution or at
-//  https://www.openssl.org/source/license.html
+//  https://github.com/openssl-projects/openssl-jostle/blob/main/LICENSE
 
 #include "block_cipher_ctx.h"
 #include <assert.h>
--- interface/util/mlkem.h
+++ interface/util/mlkem.h
@@ -1,11 +1,11 @@
 
 
-//  Copyright 2005-2025 The OpenSSL Project Authors. All Rights Reserved.
+//  Copyright 2025 OpenSSL Jostle Authors. All Rights Reserved.
 //
 //  Licensed under the Apache License 2.0 (the "License"). You may not use
 //  this file except in compliance with the License.  You can obtain a copy
 //  in the file LICENSE in the source distribution or at
-//  https://www.openssl.org/source/license.html
+//  https://github.com/openssl-projects/openssl-jostle/blob/main/LICENSE
 
 #ifndef MLKEM_H
 #define MLKEM_H
