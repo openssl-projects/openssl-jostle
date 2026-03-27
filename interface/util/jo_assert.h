972ba04cdc705f9832ba4179e763786c380b7812
mwcw <megan@bouncycastle.org>
2026-03-28 00:28:17 +1100

    Update Copyright notices

--- interface/util/jo_assert.h
+++ interface/util/jo_assert.h
@@ -1,4 +1,13 @@
 
+//
+//   Copyright 2026 OpenSSL Jostle Authors. All Rights Reserved.
+//
+//   Licensed under the Apache License 2.0 (the "License"). You may not use
+//   this file except in compliance with the License.  You can obtain a copy
+//   in the file LICENSE in the source distribution or at
+//   https://github.com/openssl-projects/openssl-jostle/blob/main/LICENSE
+//
+
 #ifndef HARD_ASSERT_H
 #define HARD_ASSERT_H
 
--- jostle/src/main/java9/org/openssl/jostle/jcajce/provider/slhdsa/SLHDSASignatureSpi.java
+++ jostle/src/main/java9/org/openssl/jostle/jcajce/provider/slhdsa/SLHDSASignatureSpi.java
@@ -1,3 +1,14 @@
+/*
+ *
+ *   Copyright 2026 OpenSSL Jostle Authors. All Rights Reserved.
+ *
+ *   Licensed under the Apache License 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License.  You can obtain a copy
+ *   in the file LICENSE in the source distribution or at
+ *   https://github.com/openssl-projects/openssl-jostle/blob/main/LICENSE
+ *
+ */
+
 package org.openssl.jostle.jcajce.provider.slhdsa;
 
 import org.openssl.jostle.disposal.NativeDisposer;
